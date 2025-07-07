7b265f5a05b9ccdb310cf263b44ded6522d4d830
Yuval Schnitzer <yuvals@tamnoon.io>
2025-07-07 17:30:18 -0400

    with testing that doesn't work

--- src/main.c
+++ src/main.c
@@ -5,6 +5,21 @@
 #include <unistd.h>
 
 int main() {
+  HashMap hashmap = createHashmap();
+  FieldData data = {.data = "something", .type = VARCHAR};
+  FieldData data2 = {.data = "goodDay", .type = VARCHAR};
+  int statusCode = hashmapSetFieldData(&hashmap, "1", data);
+  printf("status: %d\n", statusCode);
+  statusCode = hashmapSetFieldData(&hashmap, "2", data2);
+  printf("status: %d\n", statusCode);
+
+  FieldData returnData = {.data = "", .type = VARCHAR};
+  statusCode = hashmapGetByKey(&hashmap, "1", &returnData);
+  printf("1: %s\n", returnData.data);
+  hashmapGetByKey(&hashmap, "2", &returnData);
+  printf("2: %s\n", returnData.data);
+  hashmapGetByKey(&hashmap, "3", &returnData);
+  printf("3: %s\n", returnData.data);
   // FieldData data1 = {.data = "something", .type = VARCHAR};
   // HashMapEntry entry1 = {.key = "something", .next = NULL, .value = data1};
   // FieldData data2 = {.data = "something2", .type = VARCHAR};
