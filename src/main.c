8faea8800ef587127b70f3bff39d1c097009afc5
Yuval Schnitzer <yuvals@tamnoon.io>
2025-10-07 21:39:01 -0400

    parsing list of integers

--- src/main.c
+++ src/main.c
@@ -7,5 +7,25 @@
 
 int main (int argc, const char** argv) {
     assert (argc == 2);
+    const char* input = argv[1];
+    List* list        = createList ();
+    assert ((int)input[0] == 91);
+    for (int i = 1; i < strlen (input); i++) {
+        int ascii = (int)input[i];
+        if (ascii >= 48 && ascii <= 57) { // check if input[i] is a digit
+            int number = 0;
+            while (ascii >= 48 && ascii <= 57) {
+                number *= 10;
+                number += ascii - 48;
+                i++;
+                ascii = input[i];
+            }
+            addValueToList (list,
+            createEntryValue (createSuperPrimitiveInt (number), SUPER_PRIMITIVE));
+        }
+        assert (ascii == 44 || ascii == 93 || ascii == 32 ||
+        ascii == 10); // check if input[i] is , ] whitespace \n
+    }
+    printListln (list);
     return 0;
 }
